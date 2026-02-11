772f5d1c9b6599718ca5647c206ccdedda8b43af
laurajennings <laura.grace96@hotmail.com>
2026-02-11 15:51:37 +1000

    Interface & form setup

---
+++ Civ6Planner/Views/ILoadGameView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civ6Planner.Views
+{
+    public interface ILoadGameView
+    {
+        string SearchName { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler DeleteClicked;
+        event EventHandler CancelClicked;
+        event EventHandler LoadClicked;
+
+        void SetGameBindingSource(BindingSource gamesList);
+        void Show();
+    }
+}
---
+++ Civ6Planner/Views/LoadGameView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Civ6Planner.Views
+{
+    public partial class LoadGameView : Form, ILoadGameView
+    {
+        public LoadGameView()
+        {
+            InitializeComponent();
+        }
+
+        public string SearchName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSuccessful { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler DeleteClicked;
+        public event EventHandler CancelClicked;
+        public event EventHandler LoadClicked;
+
+        public void SetGameBindingSource(BindingSource gamesList)
+        {
+            dataGridGames.DataSource = gamesList;
+        }
+    }
+}
