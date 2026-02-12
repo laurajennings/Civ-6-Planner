bf5faad30a0d80e161b25f6d971631797badd3bb
laurajennings <laura.grace96@hotmail.com>
2026-02-12 14:58:32 +1000

    Check db file exists

---
+++ Civ6Planner/_Repos/Seed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civ6Planner._Repos
+{
+    public class Seed : BaseRepo
+    {
+        private readonly string _connectionString;
+        private readonly string csvPathCivs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "civs.csv");
+
+        public Seed(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CheckDbExists()
+        {
+            var builder = new SQLiteConnectionStringBuilder(_connectionString);
+            string dbPath = builder.DataSource;
+
+            if (!File.Exists(dbPath))
+            {
+                Debug.WriteLine("db doesn't exist");
+            }
+        }
+    }
+}
